e91ccebaeae1d7a446918bf8f4440d9330e3acb6
Jesus Conde <educaleft@gmail.com>
2021-12-09 19:39:28 +0000

    Agregar archivos de proyecto.

---
+++ Vinculaciones/Vinculaciones.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+int main()
+{ // bloque externo
+
+    int x{ 5 }; // x entra en alcance y es creado aquí
+
+    { // bloque anidado
+        int y{ 7 }; // y entra en alcance y es creado aquí
+
+        // x e y están ambos en alcance aquí
+        std::cout << x << " + " << y << " = " << x + y << '\n';
+    } // y va fuera de alcance y es destruido aquí
+
+    // y ya no puede ser usado aquí porque está fuera de alcance en este bloque externo
+
+    return 0;
+} // x va fuera de alcance y es destruido aquí
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*int main() // bloque externo
+{
+    int x{ 5 }; // x entra en alcance y es creado aquí
+
+    { // bloque anidado
+        int y{ 7 }; // y entra en alcance y es creado aquí
+    } // y sale de alcance y es destruido aquí
+
+    // y no puede ser usado aquí porque está fuera de alcance en este bloque
+
+    return 0;
+} // x va fuera de alcance y es destruido aquí*/
+
